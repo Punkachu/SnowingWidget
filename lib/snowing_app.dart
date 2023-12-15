aeaff61025c251ecfe1f5e676f158b52552f441d
Punkachu <ovila.lugard@gmail.com>
2023-12-16 00:46:23 +0100

    🐛 remove unneeded MaterialApp widget

--- lib/snowing_app.dart
+++ lib/snowing_app.dart
@@ -6,34 +6,32 @@ class SnowingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xFF002C7E),
-                Color(0xFF0D0C2F),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: SnowWidget(
-                  isRunning: true,
-                  totalSnow: 50,
-                  speed: 0.2,
-                  maxRadius: 8,
-                  snowColor: Colors.white,
-                ),
-              ),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF002C7E),
+              Color(0xFF0D0C2F),
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
+        ),
+        child: const Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: SnowWidget(
+                isRunning: true,
+                totalSnow: 50,
+                speed: 0.2,
+                maxRadius: 8,
+                snowColor: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
